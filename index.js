6ad4171b334d2a3c6be213c165d83641eea36985
JorgeMartinez <jotamartinez16@gmail.com>
2022-07-14 22:45:25 -0500

    probamos texto en h1

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express()
 
 app.get('/', (req, res)=>{
   console.log('peticion recibida')  
-  res.status(200).send("<h1>probando server, NODEMON</h1>")
+  res.status(200).send("<h1> probando el server en el 4000, ... </h1>")
 })
 
 app.listen(4000,()=>{
     console.log("escuchando en el 4000")
 })
+
