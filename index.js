e00bee78ae59a03796fbd029bbf21ec46a29d23b
JorgeMartinez <jotamartinez16@gmail.com>
2022-07-18 18:07:41 -0500

    conel env

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express =require('express');
-const app = express()
+const app = express();
+require('dotenv').config();
+
 
 app.get('/', (req, res)=>{
   console.log('peticion recibida')  
   res.status(200).send("<h1> probando el server en el 4000, ... </h1>")
 })
 
-app.listen(4000,()=>{
-    console.log("escuchando en el 4000")
+const PORT = process.env.PORT || 4000; 
+
+app.listen(PORT,()=> {
+    console.log(`listening on port ${PORT}`)
 })
 
