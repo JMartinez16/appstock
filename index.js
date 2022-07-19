e30d4e9df616e5e5e48820b0b247a0545fed194f
JorgeMartinez <jotamartinez16@gmail.com>
2022-07-18 19:54:25 -0500

    prueba br

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 app.get('/', (req, res)=>{
   console.log('peticion recibida')  
-  res.status(200).send("<h1> probando el server en el 4000, ... </h1>")
+  res.status(200).send("<h1> probando backend creado, creando repo y conectandolo con Azure. <br> sefue asi </br> </h1>")
 })
 
 const PORT = process.env.PORT || 4000; 
